b58afb4b5f399e09f4a8656ddace012eeb358aba
jpryor812 <85300205+jpryor812@users.noreply.github.com>
2024-09-22 18:06:57 -0400

    Update find-my-friends-button.tsx

--- components/find-my-friends-button.tsx
+++ components/find-my-friends-button.tsx
@@ -3,22 +3,52 @@ import styles from "./find-my-friends-button.module.css";
 
 export type FindMyFriendsButtonType = {
   className?: string;
+  frameDivWidth?: string;
+  findMyFriendsMargin?: string;
+  findMyFriendsWidth?: string;
+  findMyFriendsFontWeight?: string;
+  emojiWavingHandSignHeight?: string;
+  emojiWavingHandSignWidth?: string;
 };
 
 const FindMyFriendsButton: NextPage<FindMyFriendsButtonType> = ({
   className = "",
+  frameDivWidth,
+  findMyFriendsMargin,
+  findMyFriendsWidth,
+  findMyFriendsFontWeight,
+  emojiWavingHandSignHeight,
+  emojiWavingHandSignWidth,
 }) => {
   return (
-    <div className={[styles.findMyFriendsButton, className].join(" ")}>
-      <div className={styles.findMyFriendsButtonChild} />
+    <div
+      className={[styles.findMyFriendsButton, className].join(" ")}
+      style={{ width: frameDivWidth }}
+    >
+      <div
+        className={styles.findMyFriendsButtonChild}
+        style={{ margin: findMyFriendsMargin }}
+      />
       <div className={styles.findMyFriendsWrapper}>
-        <h2 className={styles.findMyFriends}>Find My Friends</h2>
+        <h2
+          className={styles.findMyFriends}
+          style={{
+            width: findMyFriendsWidth,
+            fontWeight: findMyFriendsFontWeight,
+          }}
+        >
+          Find My Friends
+        </h2>
       </div>
       <img
         className={styles.emojiWavingHandSign}
         loading="lazy"
         alt=""
         src="/-emoji-waving-hand-sign1.svg"
+        style={{
+          height: emojiWavingHandSignHeight,
+          width: emojiWavingHandSignWidth,
+        }}
       />
     </div>
   );
